399c9600f8d2b0acb882621f38f2367fc907f6b7
Paolodaolio <56475316+Paolodaolio@users.noreply.github.com>
2020-12-10 18:46:46 +0100

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+import csv
+
 class Race:
     def __init__(self, position,time,runner,level,club,race,date):
         self.position = position
@@ -8,3 +10,31 @@ class Race:
         self.race = race
         self.date = date
 
+    def to_csv(self):
+        list = []
+        list.append(self.position)
+        list.append(self.time)
+        list.append(self.runner)
+        list.append(self.level)
+        list.append(self.club)
+        list.append(self.race)
+        list.append(self.date)
+        return list
+
+
+
+class Runner:
+    races = []
+    def __init__(self,name):
+        self.name = name.upper()
+
+    def append_races(self, races):
+        for race in races:
+            if race.runner == self.name:
+                self.races.append(race)
+
+    def to_csv(self):
+        with open(str(self.name)+'.csv', 'a', newline='') as file:
+            for race in self.races:
+                write = csv.writer(file)
+                write.writerow(race.to_csv())
