367d12b56f059c889d4def369d2e7aa5ed9724e4
Paolodaolio <56475316+Paolodaolio@users.noreply.github.com>
2020-12-10 17:52:55 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,10 @@
+class Race:
+    def __init__(self, position,time,runner,level,club,race,date):
+        self.position = position
+        self.time = time
+        self.runner = runner
+        self.level = level
+        self.club = club
+        self.race = race
+        self.date = date
+
